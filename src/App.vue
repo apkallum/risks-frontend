6e73161e16e293392dec3f27b465b68d6806af24
Apkallum Enki <apkallum@protonmail.com>
2018-02-11 01:35:10 +0000

    Dynamic API calls and population of field implemented

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
 <b-container>
 <app-header></app-header>
-<app-selectrisk></app-selectrisk>
-<app-riskfields :riskFieldsList = "risksObject.RiskTypeGenericField"></app-riskfields>
+<app-riskfields></app-riskfields>
 <app-footer></app-footer>
 </b-container>
 </template>
 
 <script>
-import axios from 'axios';
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
-import SelectRisk from './components/SelectRisk.vue';
 import RiskFields from './components/RiskFields.vue';
 export default {
     components: {
         'app-header': Header,
-        'app-selectrisk': SelectRisk,
         'app-riskfields': RiskFields,
         'app-footer': Footer
 
@@ -24,35 +20,11 @@ export default {
   name: 'app',
   data () {
     return {
-      risksObject: {
-    "uri": "http://localhost:8000/api/risktype/1/",
-    "User": 1,
-    "Title": "RT1U1",
-    "RiskTypeGenericField": [
-        {
-            "pk": 3,
-            "Type": "Text",
-            "FieldTitle": "GenericField1RT1U1",
-            "RiskTypeTextFieldValue": {
-                "Value": "The Zoo"
-            }
-        },
-        {
-            "pk": 4,
-            "Type": "Number",
-            "FieldTitle": "GenericField2RT1U1",
-            "RiskTypeNumberFieldValue": {
-                "Value": 9000
-            }
-        }
-    ]
-},
+
     }
   },
   created () {
-    // Assuming a logged in user return their RiskTypes & associated RiskTypeFields
-    // axios.get('https://swapi.co/api/people/1')
-    // .then(response => console.log(response.data))
+
   }
 }
 </script>
--- src/components/RiskFields.vue
+++ src/components/RiskFields.vue
@@ -1,22 +1,32 @@
 <template>
-<b-container>
-<b-row class="my-1" v-for="field in riskFieldsList" :key="field">
-    <b-col sm="3"><label>{{ field.FieldTitle }}:</label></b-col>
-    <b-col sm="9"><b-form-input :id="`type-${field.Type.toLowerCase()}`" :type="field.Type.toLowerCase()" :value="getValueOfField(field)"></b-form-input></b-col>
+<b-container fluid>
+<b-row>
+ <b-dropdown id="ddown1" text="Select Risk Type" class="m-md-2">
+<b-dropdown-item v-for="(riskType, index) in allRiskTypes" :key="index" :value="riskType.Title" @click="currentRiskType = riskType">
+{{riskType.Title}}
+</b-dropdown-item>
+ </b-dropdown>
+<div class="m-md-4" v-if="currentRiskType">{{currentRiskType.Title}}</div>
+</b-row>
+
+<b-row class="my-1" v-for="(field, index) in currentRiskType.RiskTypeGenericField" :key="index">
+    <b-col sm="5"><b-input :value="field.FieldTitle"></b-input></b-col>
+    <b-col sm="7"><b-input :id="`type-${field.Type.toLowerCase()}`" :type="field.Type.toLowerCase()" :value="getValueOfField(field)"></b-input></b-col>
 </b-row>
 </b-container>
 </template>
 
 <script>
+import axios from 'axios';
     export default {
-        props: ['riskFieldsList'],
         data () {
             return {
-
+                    allRiskTypes: [],
+                    currentRiskType: {}
             }
         },
         methods: {
-            getValueOfField: function(Field){
+            getValueOfField: (Field) => {
                 if (Field.Type === 'Text') {
                     return Field.RiskTypeTextFieldValue.Value;
                 }
@@ -29,14 +39,30 @@
                 if (Field.Type === 'Enum') {
                     return Field.RiskTypeEnumFieldValue.Value;
                 }
-            }
-                    
-        }
+            },
+            addNewFieldToRisk: (riskObject) => {
+                this.riskObject.RiskTypeGenericField.push (
+                    {
+                        
+                    }
+                )
+            },
+               
+        },
+        // Add method to extract all riskObjects from API response 
+        created () {
+    // Assuming a logged in user return their RiskTypes & associated RiskTypeFields
+    axios.get('http://backend.bcoreproject.info/api/risktype/?format=json')
+   .then(response => this.allRiskTypes = response.data)
+
+//  .then(response => console.log(response.data))
+
+  }
                 
     }
            
        
-    
+
 
 </script>
 
