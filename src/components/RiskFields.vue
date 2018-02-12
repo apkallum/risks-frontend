4f729d70ef0fe4fdfdf2cf26945308dfb24a13c9
Apkallum Enki <apkallum@protonmail.com>
2018-02-12 11:35:49 +0000

    Consumes new API, UI cleanup, and added buttons to add fields

--- src/components/RiskFields.vue
+++ src/components/RiskFields.vue
@@ -1,27 +1,30 @@
 <template>
 <b-container fluid>
 <b-row>
- <b-dropdown id="ddown1" text="Select Risk Type" class="m-md-2">
+ <b-dropdown id="ddown1" text="Select Risk Type" class="m-sm-2">
 <b-dropdown-item v-for="(riskType, index) in allRiskTypes" :key="index" :value="riskType.Title" @click="currentRiskType = riskType">
 {{riskType.Title}}
 </b-dropdown-item>
  </b-dropdown>
-<div class="m-md-4" v-if="currentRiskType">{{currentRiskType.Title}}</div>
+ <b-dropdown id="ddown1" text="Add Field to Current Risk Type" class="m-sm-2">
+<b-dropdown-item @click="addNewFieldToRisk(currentRiskType, 'Text')">Add Text Field</b-dropdown-item>
+<b-dropdown-item @click="addNewFieldToRisk(currentRiskType, 'Number')">Add Number Field</b-dropdown-item>
+<b-dropdown-item @click="addNewFieldToRisk(currentRiskType, 'Date')">Add Date Field</b-dropdown-item>
+<b-dropdown-item @click="addNewFieldToRisk(currentRiskType, 'Enum')">Add Enum Field</b-dropdown-item>
+</b-dropdown>
+<br>
+<b-button variant="primary" size="lg" class="m-sm-2">Add New Risk Type</b-button>
 </b-row>
 
-<b-row class="my-1" v-for="(field, index) in currentRiskType.RiskTypeGenericField" :key="index">
+<b-row>
+<div class="m-sm-2" v-if="currentRiskType"><strong>{{currentRiskType.Title}}</strong></div>
+</b-row>
+
+<b-row class="my-1" v-for="(field, index) in currentRiskType.RiskTypeGenericType" :key="index">
     <b-col sm="5"><b-input v-model="field.FieldTitle"></b-input></b-col>
-    <b-col sm="7"><b-input :id="`type-${field.Type.toLowerCase()}`" :type="field.Type.toLowerCase()" :value="getValueOfField(field)"></b-input></b-col>
+    <b-col sm="7"><b-input :id="`type-${field.Type.toLowerCase()}`" :type="field.Type.toLowerCase()" v-model="field.FieldValue"></b-input></b-col>
 </b-row>
 <br><br>
-<b-row>
-<b-btn-group>
-<b-button>Add Text Field</b-button>
-<b-button>Add Number Field</b-button>
-<b-button>Add Date Field</b-button>
-<b-button>Add Enum Field</b-button>
-</b-btn-group>
-</b-row>
 <br><br>
 </b-container>
 </template>
@@ -38,61 +41,22 @@ import axios from 'axios';
             }
         },
         methods: {
-            getValueOfField: (Field) => {
-                if (Field.Type === 'Text') {
-                    return Field.RiskTypeTextFieldValue.Value;
-                }
-                if (Field.Type === 'Number') {
-                    return Field.RiskTypeNumberFieldValue.Value;
-                }
-                if (Field.Type === 'Date') {
-                    return Field.RiskTypeDateFieldValue.Value;
-                }
-                if (Field.Type === 'Enum') {
-                    return Field.RiskTypeEnumFieldValue.Value;
-                }
-            },
-            addNewFieldToRisk: (riskObject) => {
-                this.riskObject.RiskTypeGenericField.push (
+            addNewFieldToRisk: (riskObject, Type) => {
+                riskObject.RiskTypeGenericType.push (
                     {
-                        
+                        'Type': Type,
+                        'FieldTitle': '',
+                        'FieldValue': ''
                     }
                 )
-            },
-            getNameOfFieldValue: function(Type) {
-                if (Type === 'Text') {
-                    return 'RiskTypeTextFieldValue';
-                }
-                if (Type === 'Number') {
-                    return 'RiskTypeNumberFieldValue';
-                }
-                if (Type === 'Date') {
-                    return 'RiskTypeDateFieldValue';
-                }
-                if (Type === 'Enum') {
-                    return 'RiskTypeEnumFieldValue';
-                }
-            },
-            unifyFieldValueNames: function(allRiskTypes){
-                for (var i = 0, len1 = allRiskTypes.length; i < len1; i++) {
-                    for (var j = 0, len2 = allRiskTypes[i].length; j < len2; j++) {
-                        allRiskTypes[i].RiskTypeGenericField[j][RiskTypeFieldValue] = allRiskTypes[i].RiskTypeGenericField[j][getNameOfFieldValue(allRiskTypes[i].RiskTypeGenericField[j].Type)]
-                        delete allRiskTypes[i].RiskTypeGenericField[j][getNameOfFieldValue(allRiskTypes[i].RiskTypeGenericField[j].Type)]    
-                    }
-                }                
-            }    
+            },    
         },
         created () {
     // Assuming a logged in user return their RiskTypes & associated RiskTypeFields
             axios.get('http://backend.bcoreproject.info/api/risktype/?format=json')
             .then(response => this.allRiskTypes = response.data)
 
-  },
-        mounted () {
-            this.unifyFieldValueNames(this.allRiskTypes)
-            }
-
-            
+  },        
     }
            
        
